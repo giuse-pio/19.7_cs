83c969046a6262b32dfea6be01c52eaee36edbdb
giupe <giupe@MSI>
2025-09-19 18:26:48 +0200

    Add project files.

---
+++ 19.7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _19._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomi = new List<string> { "filippo", "anna", "claudio", "alessia", "mirko", "giacomo" };
+            Console.WriteLine("inserisci il nome di un partecipante");
+            string nome = Console.ReadLine();
+            nome = nome.ToLower();
+            int posizione = -1;
+            for (int i = 0; i < nomi.Count; i++)
+            {
+                if (nomi[i] == nome)
+                    posizione = i;
+            }
+            if(posizione==-1)
+                Console.WriteLine("il nome inserito non è presente nella lista, ed è in posizione {0}");
+            else
+                Console.WriteLine("il nome inserito è presente nella lista, ed è in posizione {0}", posizione + 1);
+
+
+        }
+    }
+}
